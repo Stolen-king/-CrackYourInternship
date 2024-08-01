2e0fc0c3c826f0b369a3b09d72d44e404ca2ddb5
Stolen-king <aryananandsaha499@gmail.com>
2024-08-01 22:31:22 +0530

    fifty seven

---
+++ 057_minimum_moves_to_make_array_element_equal_ii.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minMoves2(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int md = (nums[nums.size()/2]);
+        int cnt=0;
+
+        for(int i=0;i<nums.size();i++){
+            cnt+=abs(md-nums[i]);
+        }
+        return cnt;
+    }
+};
